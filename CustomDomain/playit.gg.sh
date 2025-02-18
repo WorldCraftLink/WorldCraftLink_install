2c1309c4498ea1b243fdc432aa25aebeae7062e5
ItsGamer2o23 <itsgamerluffy@gmail.com>
2025-02-18 02:10:41 -0800

    Add more etc

---
+++ CustomDomain/playit.gg.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+clear
+
+# Custom 
+NC="\033[0m"
+BOLD="\033[01m"
+DISABLE="\033[02m"
+
+#Coler
+BLUE="\033[1;34m" #ONLY INFO
+RED="\033[0;91m" #ONLY 
+DARKGREY="\033[90m"
+WHITE="\033[37m"
+
+echo -e "${WHITE}
+
+██╗    ██╗██████╗██████╗██╗    ██████╗ ████████████╗ █████╗███████████████╗
+██║    ████╔═══████╔══████║    ██╔══████╔════██╔══████╔══████╔════╚══██╔══╝
+██║ █╗ ████║   ████████╔██║    ██║  ████║    ██████╔████████████╗    ██║   
+██║███╗████║   ████╔══████║    ██║  ████║    ██╔══████╔══████╔══╝    ██║   
+╚███╔███╔╚██████╔██║  ███████████████╔╚████████║  ████║  ████║       ██║   
+ ╚══╝╚══╝ ╚═════╝╚═╝  ╚═╚══════╚═════╝ ╚═════╚═╝  ╚═╚═╝  ╚═╚═╝       ╚═╝   
+                        ██╗    █████╗   ████╗  ██╗                             
+                        ██║    ██████╗  ████║ ██╔╝                             
+                        ██║    ████╔██╗ ███████╔╝                              
+                        ██║    ████║╚██╗████╔═██╗                              
+                        ███████████║ ╚██████║  ██╗                             
+                        ╚══════╚═╚═╝  ╚═══╚═╝  ╚═╝                             
+                                                                           
+                      Author: WorldCraftLink
+                      Last Updated: 2024-12-10"
+echo -e "${BOLD}${BLUE}[INFO] ${DISABLE}This create a free vps${NC}"
+    echo -e "${BOLD}${BLUE}[INFO] ${DISABLE}Select an option:${NC}"
+    echo -e "▶ 1 ${BOLD}${WHITE}| YES"
+    echo -e "▶ 2 ${BOLD}${WHITE}| BACK"
+    read option_freevps
+            
+        if [ $option_freevps -eq 1 ]; then
+            clear
+            echo -e "${WHITE}
+
+██╗    ██╗██████╗██████╗██╗    ██████╗ ████████████╗ █████╗███████████████╗
+██║    ████╔═══████╔══████║    ██╔══████╔════██╔══████╔══████╔════╚══██╔══╝
+██║ █╗ ████║   ████████╔██║    ██║  ████║    ██████╔████████████╗    ██║   
+██║███╗████║   ████╔══████║    ██║  ████║    ██╔══████╔══████╔══╝    ██║   
+╚███╔███╔╚██████╔██║  ███████████████╔╚████████║  ████║  ████║       ██║   
+ ╚══╝╚══╝ ╚═════╝╚═╝  ╚═╚══════╚═════╝ ╚═════╚═╝  ╚═╚═╝  ╚═╚═╝       ╚═╝   
+                        ██╗    █████╗   ████╗  ██╗                             
+                        ██║    ██████╗  ████║ ██╔╝                             
+                        ██║    ████╔██╗ ███████╔╝                              
+                        ██║    ████║╚██╗████╔═██╗                              
+                        ███████████║ ╚██████║  ██╗                             
+                        ╚══════╚═╚═╝  ╚═══╚═╝  ╚═╝                             
+                                                                           
+                      Author: WorldCraftLink
+                      Last Updated: 2024-12-10"
+        wget https://github.com/playit-cloud/playit-agent/releases/download/v0.15.26/playit-linux-amd64
+        chmod +x playit-linux-amd64
+        ./playit-linux-amd64
+        echo -e "${BOLD}${BLUE}[INFO] ${DISABLE}click the link
+        elif [ $option_freevps -eq 2 ]; then
+        #bash <(curl -s https://raw.githubusercontent.com/WorldCraftLink/WorldCraftLink_install/refs/heads/main/CustomDomain_SD.sh)
+else
+  echo -e "${BOLD}${RED}[ERROR] ${DISABLE}Invalid option selected.${NC}" #FreeVPS
+fi
--- InstallSD/CustomDomain_SD.sh
+++ InstallSD/CustomDomain_SD.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 clear
-#
+
+# Custom 
 NC="\033[0m"
 BOLD="\033[01m"
 DISABLE="\033[02m"
+
 #Coler
 BLUE="\033[1;34m" #ONLY INFO
 RED="\033[0;91m" #ONLY 
@@ -27,21 +29,18 @@ echo -e "${WHITE}
                                                                            
                       Author: WorldCraftLink
                       Last Updated: 2024-12-10"
-echo -e "${BOLD}${BLUE}[INFO] ${DISABLE}Select an option:${NC}"
-echo -e "▶ 1${BOLD}${WHITE} | Install Free VPS${NC}"
-echo -e "▶ 2${BOLD}${WHITE} | Host Server${NC}"
-echo -e "▶ 3${BOLD}${WHITE} | Custom Domain${NC}"
-read option
-
-if [ $option -eq 1 ]; then
-    sleep 1
-    bash <(curl -s https://raw.githubusercontent.com/WorldCraftLink/WorldCraftLink_install/refs/heads/main/InstallSD/FreeVPS_SD.sh)
-elif [ $option -eq 2 ]; then
-    sleep 1
-    bash <(curl -s https://raw.githubusercontent.com/WorldCraftLink/WorldCraftLink_install/refs/heads/main/InstallSD/HostServer_SD.sh)
-elif [ $option -eq 3 ]; then
-    #bash <(curl -s https://raw.githubusercontent.com/WorldCraftLink/WorldCraftLink_install/refs/heads/main/InstallSD/CustomDomain_SD.sh)
-    sleep 1
-      else
-   echo -e "${BOLD}${RED}[ERROR] ${DISABLE}Invalid option selected.${NC}"
-fi
+echo -e "${BOLD}${BLUE}[INFO] ${DISABLE}This create a free customdomain name.${NC}"
+    echo -e "${BOLD}${BLUE}[INFO] ${DISABLE}Select an option:${NC}"
+    echo -e "▶ 1 ${BOLD}${WHITE}| Playit.gg${NC}"
+    echo -e "▶ 2 ${BOLD}${WHITE}| Cloudflare${NC}"
+    echo -e "▶ 3${BOLD}${WHITE} | BACK${NC}"
+    read option_CustomDomain
+        if [ $option_CustomDomain -eq 1 ]; then
+        bash <(curl https://raw.githubusercontent.com/WorldCraftLink/WorldCraftLink_install/refs/heads/main/CustomDomain/playit.gg.sh)
+        elif [ $option_CustomDomain -eq 2 ]; then
+        bash <(curl https://raw.githubusercontent.com/WorldCraftLink/WorldCraftLink_install/refs/heads/main/CustomDomain/Cloudflare.sh)
+        elif [ $option_CustomDomain -eq 3 ]; then
+        bash <(curl -s https://raw.githubusercontent.com/WorldCraftLink/WorldCraftLink_install/refs/heads/main/Script.sh)
+               else
+          echo -e "${BOLD}${RED}[ERROR] ${DISABLE}Invalid option selected.${NC}"
+   fi
